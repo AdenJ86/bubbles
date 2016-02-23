501ddd0804adac4ba1c06a266eda95341492916a
AdenJ86 <mradenjordan@gmail.com>
2016-02-22 19:28:26 -0500

    Paste 500 bubbles code in.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
 var bubbles = [];
 
+var addNumBubbles = function (totalBubbles) {
+  for (var i = 0; i < totalBubbles; i++) {
+    bubbles[i] = new Bubble(random(0, width),
+    random(0, height));
+  }
+};
+
 setup = function () {
   createCanvas(600, 600);
   // your code goes here
+  addNumBubbles(500);
 };
 
 draw = function () {
   background(0);
-  // your code goes here
+
+  bubbles.forEach(function updateAndDisplay(bubble) {
+    bubble.update();
+    bubble.display();
+  });
 };
