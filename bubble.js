db3c58d6b71c5394ec79276483ec55d1c1565b81
AdenJ86 <mradenjordan@gmail.com>
2016-02-24 14:10:27 -0500

    Made snow fall
    Made snow fall

--- bubble.js
+++ bubble.js
@@ -1,21 +1,23 @@
-var Bubble = function (x, y) {
+Bubble:
+var Snow = function (x, y) {
   this.x = x;
   this.y = y;
+  this.speed = {x: 1, y:1};
 };
 
-Bubble.prototype = {
+Snow.prototype = {
 
-  radius: 5,
+  radius: 2,
 
   update: function() {
     this.x += random(-1, 1);
-    this.y += random(0, -2);
+    this.y += random(0, 2);
     if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
   },
 
   display: function () {
     noStroke();
-    fill(102, 217, 255, 100);
+    fill(250);
     ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
   }
 
--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
-var bubbles = [];
+Sketch:
+var snowfall = [];
+
+//var myBubble = new Bubble(100, 100);
+//myBubble.speed.x = 2;
+//myBubble.speed.y = 2;
 
 setup = function () {
-  createCanvas(600, 600);
-  // your code goes here
+  createCanvas(600, 400);
+  iterateWithForLoop(500);
 };
 
 draw = function () {
-  background(0);
-  // your code goes here
+  background(100);
+
+  snowfall.forEach(function updateAndDisplay(Snow) {
+    Snow.display();
+    Snow.update();
+  });
+};
+
+var iterateWithForLoop = function (numberOfSnow) {
+  for (var i = 0; i < numberOfSnow; ++i) {
+    snowfall.push (new Snow (random(0, width-10), random(10, height - 10)));
+    //updateAndDisplay(array[i]);
+  }
 };
